0bcee67bba17c562d52a73dd9e1dd8dec754f1de
Kiran Joshi <kiran@karmacircles.com>
2018-03-27 15:43:43 +0530

    Partially completed structure of about blade and css

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,66 +1,115 @@
 @extends('common.master')
 @section('content')
-<section class="hp-container" style="background-color:white;">
+<section class="about-container">
   	<div class="container-fluid">
     	<div>
-      	<div class="hp-heroImg" style="background-image:url('/images/aboutPage/Heroimage_About.png');height:730px;width:102.2%;margin-left:-1.1em;">
-      		<h1 style="color:white;padding:3em 0 0 3em;margin-top:0;font-size:4em">AN INSIGHT</h1> 
-      		<h1 style="color:white;padding:0 0 0 4em;margin-top:0;font-size:3em">Prasad's Life and Contributions</h1>
+      	<div class="about-heroImg">
+      		<h1>AN INSIGHT</h1> 
+      		<h2>Prasad's Life and Contributions</h2>
       		<br><br><br><br><br><br><br><br><br><br><br>
-      		<div class="col-lg-1 col-lg-offset-1" style="height:118px;border:2px solid white;border-radius:80px;margin-right:5em;padding:2em;">
-      			<i class="fa fa-target" style="color:white;vertical-align:sub;"></i>
+      		<div class="col-lg-1 col-lg-offset-1 about-icons">
+      			<i class="material-icons"></i>
       		</div>
-      		<div class="col-lg-1" style="height:118px;border:2px solid white;border-radius:80px;margin-right:5em;padding:2em;">
-      			<i class="fa fa-road fa-4x" style="color:white;"></i>
+      		<div class="col-lg-1 about-icons">
+      			<i class="fa fa-road fa-4x"></i>
       		</div>
-      		<div class="col-lg-1" style="height:118px;border:2px solid white;border-radius:80px;margin-right:5em;padding:2em 1.6em;">
-      			<i class="fa fa-handshake-o fa-4x" style="color:white;"></i>
+      		<div class="col-lg-1 about-icons" style="padding:2em 1.6em;">
+      			<i class="fa fa-handshake-o fa-4x"></i>
       		</div>
-      		<div class="col-lg-1" style="height:118px;border:2px solid white;border-radius:80px;margin-right:5em;padding:2.6em 2.8em;">
-      			<i class="fa fa-microphone fa-4x" style="color:white;"></i>
+      		<div class="col-lg-1 about-icons" style="padding:2.6em 2.8em;">
+      			<i class="fa fa-microphone fa-4x"></i>
       		</div>
       	</div>
     	</div>
-    	<div class="row" style="margin-top:6em;">
-    		<div class="col-lg-6 col-lg-offset-1">
+    	<div class="row about-mdesignRow">
+    		<div class="col-lg-6 col-lg-offset-1 about-mdesign">
         		<h2 align="center">PRASAD'S PURPOSE</h2>
-        		<h3 align="center" style="margin-top:-0.2em;">Thought Provoker.</h3>
-        		<p style="font-size:16px;text-align:justify;">Prasad has worn many hats in his journey spanning decades. He is an author, leadership coach, adviser,
+        		<h3 align="center">Thought Provoker.</h3>
+        		<p>Prasad has worn many hats in his journey spanning decades. He is an author, leadership coach, adviser,
 				researcher, father and husband - in no particular order. Amidst changing roles, the only constant in his
 				life has been a desire to delve deeper into his essence and sensibility.</p>
-				<p style="font-size:16px;text-align:justify;">Prasad strives to bring Indian Vedantic knowledge into management theory and practice. Just like Japanese management contribution to manufacturing, he believes Indian wisdom can contribute to change management, leadership development, employee engagement and motivation in addition to general management.</p>
-				<p style="font-size:16px;text-align:justify;">Artificial Intelligence is something Prasad has seen ‘grow up’ – and has been hooked on to, ever since his days at Apple University. 
+				<p>Prasad strives to bring Indian Vedantic knowledge into management theory and practice. Just like Japanese management contribution to manufacturing, he believes Indian wisdom can contribute to change management, leadership development, employee engagement and motivation in addition to general management.</p>
+				<p>Artificial Intelligence is something Prasad has seen ‘grow up’ – and has been hooked on to, ever since his days at Apple University. 
 				AI today is so much more than it was 20 years ago; it has the potential to give humans the emotional support 	and vulnerability shield that they so need. His current passion project is to find ways to instill compassion into Alexa, Siri, Cortana and their friends.</p>
-				<p style="font-size:16px;text-align:justify;">Prasad is always on the lookout for those who share his vision and want to collaborate with him to make these dreams a reality.</p>
+				<p>Prasad is always on the lookout for those who share his vision and want to collaborate with him to make these dreams a reality.</p>
       	</div>
-      	<div class="col-lg-5">
-        		<img src="/images/aboutPage/Purpose.png" style="margin-left:0.71em;">
+      	<div class="col-lg-5 about-mdImageRight">
+        		<img src="/images/aboutPage/Purpose.png">
       	</div>
     	</div>
-    	<br><br>
-    	<div class="row" style="margin-top:6em;">
-    		<div class="col-lg-5 col-lg-offset-0">
-        		<img src="/images/aboutPage/Journey.png" style="margin-left:-0.91em;">
+    	<div class="row about-mdesignRow">
+    		<div class="col-lg-5 col-lg-offset-0 about-mdImageLeft">
+        		<img src="/images/aboutPage/Journey.png">
       	</div>
-    		<div class="col-lg-6">
+    		<div class="col-lg-6 about-mdesign">
         		<h2 align="center">PRASAD'S JOURNEY AND CONTRIBUTIONS</h2>
-        		<h3 align="center" style="margin-top:-0.2em;">Spirituality, Technology and Business.</h3>
-        		<p style="font-size:16px;text-align:justify;">Prasad holds a Ph.D. in physics from the University of Utah, where his team built an international research laboratory. and was an early employee at Apple as a technology advisor, manager in Apple International Product Marketing, and research fellow in Apple University.</p>
-				<p style="font-size:16px;text-align:justify;">Prasad’s combines “igniting the genius within” as well as pursuing “personal mastery.” He integrates business approaches with brain research and spirituality/wisdom models to advise and coach C-level executives at Fortune 500 companies. He’s also serves as an educator/consultant in the areas of leadership, change management, innovation and management team development for companies like AT&T, Cisco, HP, Ford, Tata Steel, State Bank of India, Maersk, and Pepsi. Since 1990, he’s worked with leaders in every field including Apple, Adobe, Boeing, Disney, Microsoft, Nokia and Oracle.</p>
-				<p style="font-size:16px;text-align:justify;">At Center for Creative Leadership, Prasad was the Smith Richardson Visiting Fellow 2010-11. He has been an advisor to Business FAC of Fetzer Institute (2011-13). In 2016-17, Prasad contributed towards ‘Innovation & Knowledge Creation’ as a member of a panel for the Government of India.</p>
-				<p style="font-size:16px;text-align:justify;">Prasad has offered executive education programs at INSEAD, London Business School, Marshall School of Business, Tuck School of Business, and IIM Bangalore. He is a Clinical professor and founding Executive Director of the Center for Leadership, Innovation and Change (CLIC) at the Indian School of Business (ISB).</p>
+        		<h3 align="center">Spirituality, Technology and Business.</h3>
+        		<p>Prasad holds a Ph.D. in physics from the University of Utah, where his team built an international research laboratory. and was an early employee at Apple as a technology advisor, manager in Apple International Product Marketing, and research fellow in Apple University.</p>
+				<p>Prasad’s combines “igniting the genius within” as well as pursuing “personal mastery.” He integrates business approaches with brain research and spirituality/wisdom models to advise and coach C-level executives at Fortune 500 companies. He’s also serves as an educator/consultant in the areas of leadership, change management, innovation and management team development for companies like AT&T, Cisco, HP, Ford, Tata Steel, State Bank of India, Maersk, and Pepsi. Since 1990, he’s worked with leaders in every field including Apple, Adobe, Boeing, Disney, Microsoft, Nokia and Oracle.</p>
+				<p>At Center for Creative Leadership, Prasad was the Smith Richardson Visiting Fellow 2010-11. He has been an advisor to Business FAC of Fetzer Institute (2011-13). In 2016-17, Prasad contributed towards ‘Innovation & Knowledge Creation’ as a member of a panel for the Government of India.</p>
+				<p>Prasad has offered executive education programs at INSEAD, London Business School, Marshall School of Business, Tuck School of Business, and IIM Bangalore. He is a Clinical professor and founding Executive Director of the Center for Leadership, Innovation and Change (CLIC) at the Indian School of Business (ISB).</p>
       	</div>
     	</div>
    </div>
 </section>
-<section>
+
+<section class="about-container">
 	<div class="container-fluid">
-		<div class="row">
+		<div class="row about-affliations">
 			<h2 align="center">PRASAD'S AFFILIATION</h2>
-			<div class="col-lg-offset-1 col-lg-10" style="border:1px solid grey;">
-				
-			</div>
+			<div class="col-lg-offset-1 col-lg-10 about-logoBox">
+				<div class="row">
+               <h4 align="center">Prasad has served as a board member/advisor to several organizations</h4>
+               <div class="about-logos">
+                  <img src="/images/aboutPage/Affiliations/Grey/c7.png" class="imgMargin"> 
+                  <img src="/images/aboutPage/Affiliations/Grey/c1.png" class="imgMargin">    
+                  <img src="/images/aboutPage/Affiliations/Grey/Arevo.png">
+               </div>
+               <div align="right" class="about-logos">
+                  <img src="/images/aboutPage/Affiliations/Grey/c4.png"> 
+               </div>
+               <div class="about-logos">
+                  <img src="/images/aboutPage/Affiliations/Grey/c8.png" class="imageMargin"> 
+                  <img src="/images/aboutPage/Affiliations/Grey/c3.png" class="imgMargin">    
+                  <img src="/images/aboutPage/Affiliations/Grey/c6.png">
+               </div>
+            </div>
+			</div> 
 		</div>
+      <br><br>
+      <div class="row">
+         <div class="col-lg-offset-1 col-lg-10  about-logoBoxt">
+            <div class="row">
+               <h4 class="col-lg-8 col-lg-offset-2" align="center">These are some not-for-profit organizations whose mission aligns well with Prasad's ideology. He serves as board member/advisor to several of them.</h4>
+               <div class="about-logos">
+                  <img src="/images/aboutPage/Affiliations/Grey/c5.png" class="imgMargin"> 
+                  <img src="/images/aboutPage/Affiliations/Grey/c2.png" class="imgMargin">    
+                  <img src="/images/aboutPage/Affiliations/Grey/c9.png">
+               </div>
+            </div>
+         </div> 
+      </div>
+      <!-- <div class="row" style="margin:3em 0;">
+         <h2 align="center">INTERVIEWS WITH PRASAD</h2>
+         <div class="col-lg-offset-1 col-lg-10" style="background-color:white;height:200px;">
+            <div class="row">
+               <h4 align="center" style="margin-top:1em;">Prasad has served as a board member/advisor to several organizations</h4>
+               <div style="padding:1em 6.2em;">
+                  <img src="/images/aboutPage/Affiliations/Grey/c7.png" style="width:250px;"> 
+                  <img src="/images/aboutPage/Affiliations/Grey/c1.png" style="width:250px;margin:0 8em;">    
+                  <img src="/images/aboutPage/Affiliations/Grey/Arevo.png" style="width:250px;">
+               </div>
+               <div align="right" style="padding:1em 6.8em 0 0;">
+                  <img src="/images/aboutPage/Affiliations/Grey/c4.png" style="width:250px;"> 
+               </div>
+               <div style="padding:1em 6.2em;">
+                  <img src="/images/aboutPage/Affiliations/Grey/c8.png" style="width:250px;margin-top:-4em;"> 
+                  <img src="/images/aboutPage/Affiliations/Grey/c3.png" style="width:300px;margin:0 5.5em;">    
+                  <img src="/images/aboutPage/Affiliations/Grey/c6.png" style="width:280px;">
+               </div>
+            </div>
+         </div> 
+      </div> -->
 	</div>
 </section>
 

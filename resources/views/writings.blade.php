02578d3616318c95c61201040d807b6819236d95
Kiran Joshi <kiran@karmacircles.com>
2018-03-31 18:53:35 +0530

    Convert inline css to sass, remove temporary code used for testing hover effect

--- resources/views/writings.blade.php
+++ resources/views/writings.blade.php
@@ -2,40 +2,40 @@
 @section('content')
 <section class="writing-container">
   	<div class="container-fluid">
-    	<div class="row"  style="margin-top:8em;">
-    		<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2" style="background-color:white;">
-    			<header style="padding:1em;">
-    				<h1 align="center" style="font-size:3.8em;">BOOKS</h1>
-    				<hr style="width:7%;border:none;border-top:4px solid red;margin-top:-0.5em;" align="center">
+    	<div class="row writing-header">
+    		<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2 writing-box">
+    			<header>
+    				<h1 align="center">BOOKS</h1>
+    				<hr align="center">
     			</header>
-    			<div class="row" style="padding-bottom:4em;">
-		    		<div class="col-lg-4 col-md-4 col-sm-4 col-xs-10">
-		    			<img src="/images/writingPage/Books/Book1.png" style="padding-left:1em;">
+    			<div class="row writing-mdesign">
+		    		<div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 writing-book">
+		    			<img src="/images/writingPage/Books/Book1.png">
 		    		</div>
-		    		<div class="col-lg-7 col-lg-offset-1 col-md-8 col-md-offset-1 col-sm-6 col-sm-offset-1 col-xs-10 col-xs-offset-1">
-		    			<h2 style="font-size:2.4em;"><b>You Can: Be Smarter and Wiser</b></h2> 
-                        <h4 style="color:grey;"><i>By Prasad Kaipa and Meera Shenoy</i></h4>
-                        <h3 style="line-height:1.4em;margin-top:1em;padding-right:1.5em;font-size:1.6em;">The real-life stories profiled in this book cover entrepreneurs ranging from some in wheel chairs to visually impaired persons who build their businesses with zest and energy. It looks at CEOs who hire youth with disability and make products for this market, because it makes business sense. And it celebrates those who have converted disability in their personal lives to an opportunity to transform other vulnerable lives.</h3> 
+		    		<div class="col-lg-7 col-lg-offset-1 col-md-8 col-md-offset-1 col-sm-6 col-sm-offset-1 col-xs-10 col-xs-offset-1 writing-content">
+		    			<h2><b>You Can: Be Smarter and Wiser</b></h2> 
+                        <h4><i>By Prasad Kaipa and Meera Shenoy</i></h4>
+                        <h3>The real-life stories profiled in this book cover entrepreneurs ranging from some in wheel chairs to visually impaired persons who build their businesses with zest and energy. It looks at CEOs who hire youth with disability and make products for this market, because it makes business sense. And it celebrates those who have converted disability in their personal lives to an opportunity to transform other vulnerable lives.</h3> 
 		    		</div>
 		    	</div>
-                <div class="row" style="padding-bottom:4em;margin-top:4em;">
-                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10">
-                        <img src="/images/writingPage/Books/Book2.png" style="padding-left:1em;">
+                <div class="row writing-mdesign mdRow">
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 writing-book">
+                        <img src="/images/writingPage/Books/Book2.png">
                     </div>
-                    <div class="col-lg-7 col-lg-offset-1 col-md-8 col-md-offset-1 col-sm-6 col-sm-offset-1 col-xs-10 col-xs-offset-1">
-                        <h2 style="font-size:2.4em;"><b>From Smart to Wise</b></h2> 
-                        <h4 style="color:grey;"><i>By Prasad Kaipa and Navi Radjou</i></h4>
-                        <h3 style="line-height:1.4em;margin-top:1em;padding-right:1.5em;font-size:1.6em;">From Smart to Wise offers a new approach that balances the two styles to achieve a form of wise leadership that is both functional and smart. Drawing on inspiring real-life stories of historical and contemporary wise leaders such as Bill Gates, Warren Buffet, and even Mahatma Gandhi, the authors identify six characteristics of wise leaders and offer a practical framework to help readers develop their own style of wise leadership.</h3> 
+                    <div class="col-lg-7 col-lg-offset-1 col-md-8 col-md-offset-1 col-sm-6 col-sm-offset-1 col-xs-10 col-xs-offset-1 writing-content">
+                        <h2><b>From Smart to Wise</b></h2> 
+                        <h4><i>By Prasad Kaipa and Navi Radjou</i></h4>
+                        <h3>From Smart to Wise offers a new approach that balances the two styles to achieve a form of wise leadership that is both functional and smart. Drawing on inspiring real-life stories of historical and contemporary wise leaders such as Bill Gates, Warren Buffet, and even Mahatma Gandhi, the authors identify six characteristics of wise leaders and offer a practical framework to help readers develop their own style of wise leadership.</h3> 
                     </div>
                 </div>
-                <div class="row" style="padding-bottom:4em;margin-top:4em;">
-                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10">
-                        <img src="/images/writingPage/Books/Book3.png" style="padding-left:1em;">
+                <div class="row writing-mdesign mdRow">
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 writing-book">
+                        <img src="/images/writingPage/Books/Book3.png">
                     </div>
-                    <div class="col-lg-7 col-lg-offset-1 col-md-8 col-md-offset-1 col-sm-6 col-sm-offset-1 col-xs-10 col-xs-offset-1">
-                        <h2 style="font-size:2.4em;"><b>Discontinuous Learning: Igniting Genius Within by Aligning Self, Work, & Family (eBook)</b></h2> 
-                        <h4 style="color:grey;"><i>By Prasad Kaipa</i></h4>
-                        <h3 style="line-height:1.4em;margin-top:1em;padding-right:1.5em;font-size:1.6em;">In times of turbulence, a discontinuity, uncertainty and rapid change brings about a major shift in perspective. Discontinuous learning allows us to learn about ourselves, alters our mental models, and changes our relationship to the world.</h3> 
+                    <div class="col-lg-7 col-lg-offset-1 col-md-8 col-md-offset-1 col-sm-6 col-sm-offset-1 col-xs-10 col-xs-offset-1 writing-content">
+                        <h2><b>Discontinuous Learning: Igniting Genius Within by Aligning Self, Work, & Family (eBook)</b></h2> 
+                        <h4><i>By Prasad Kaipa</i></h4>
+                        <h3>In times of turbulence, a discontinuity, uncertainty and rapid change brings about a major shift in perspective. Discontinuous learning allows us to learn about ourselves, alters our mental models, and changes our relationship to the world.</h3> 
                     </div>
                 </div>
     		</div>
@@ -45,86 +45,81 @@
 
 <section class="writing-container">
     <div class="container-fluid">
-         <div class="row"  style="margin-top:8em;">
-            <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2" style="background-color:white;">
-                <header style="padding:1em;">
-                    <h1 align="center" style="font-size:3.8em;">PUBLISHED ARTICLES</h1>
-                    <hr style="width:7%;border:none;border-top:4px solid red;margin-top:-0.5em;" align="center">
+         <div class="row writing-header">
+            <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2 writing-box">
+                <header>
+                    <h1 align="center">PUBLISHED ARTICLES</h1>
+                    <hr align="center">
                 </header>
                 <!-- card 1 -->
-                <div class="row" style="padding-bottom:4em;">
-                    <div class="col-lg-8 col-lg-offset-2" onmouseover="myOverFunction()"  onmouseout="outFunction()" style="background-color:#f5f5f5;border:1px solid #D5D4D4;">
-                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                            <img src="/images/writingPage/PublishedArticles/CNBC.png" style="margin-left:-2.2em;width:100%;">
+                <div class="row writing-card">
+                    <div class="col-lg-8 col-lg-offset-2 writing-cardRow" onmouseover="myOverFunction()"  onmouseout="outFunction()">
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 writing-logos">
+                            <img src="/images/writingPage/PublishedArticles/CNBC.png">
                         </div>
-                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8" style="padding-top:1.4em;" id="demo3">
-                            <h4 style="line-height:1.2em;"><b>7 Business Decisions That Looked Bad but Turned Good</b></h4> 
-                            <h6 style="color:grey;">By Prasad Kaipa and Navi Radjou</h6>
+                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 writing-content" id="card1">
+                            <h4><b>7 Business Decisions That Looked Bad but Turned Good</b></h4> 
+                            <h6>By Prasad Kaipa and Navi Radjou</h6>
                         </div>
                     </div>      
                 </div> 
                 <!-- card 2 -->
-                 <div class="row" style="padding-bottom:4em;">
-                    <div class="col-lg-8 col-lg-offset-2" style="background-color:#f5f5f5;border:1px solid #D5D4D4;">
-                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10">
-                            <img src="/images/writingPage/PublishedArticles/Forbes.png" style="margin-left:-2.2em;width:100%;">
+                 <div class="row writing-card">
+                    <div class="col-lg-8 col-lg-offset-2 writing-cardRow">
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 writing-logos">
+                            <img src="/images/writingPage/PublishedArticles/Forbes.png">
                         </div>
-                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10" style="padding-top:1.4em;">
-                            <h4 style="line-height:1.2em;"><b>How to Gain Role Clarity and Lead from any Position</b></h4> 
-                            <h6 style="color:grey;">By Prasad Kaipa, Navi Radjou and Sarwath Khizrana</h6>
-                            <span>Wise Leaders are willing to play the role of a servant or a leader based on the context.</span>
+                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10 writing-content" id="card2">
+                            <h4><b>How to Gain Role Clarity and Lead from any Position</b></h4> 
+                            <h6>By Prasad Kaipa, Navi Radjou and Sarwath Khizrana</h6>
                         </div>
                     </div>      
                 </div>
                 <!-- card 3  --> 
-                 <div class="row" style="padding-bottom:4em;">
-                    <div class="col-lg-8 col-lg-offset-2" style="background-color:#f5f5f5;border:1px solid #D5D4D4;">
-                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10">
-                            <img src="/images/writingPage/PublishedArticles/HBR.png" style="margin-left:-2.2em;width:100%;">
+                 <div class="row writing-card">
+                    <div class="col-lg-8 col-lg-offset-2 writing-cardRow">
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 writing-logos">
+                            <img src="/images/writingPage/PublishedArticles/HBR.png">
                         </div>
-                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10" style="padding-top:1.4em;">
-                            <h4 style="line-height:1.2em;"><b>Watching Wise Leaders Deal With Complexity</b></h4> 
-                            <h6 style="color:grey;">By Prasad Kaipa and Navi Radjou</h6>
-                            <span>When wisdom provides the ethical compass, intelligence can become even more potent.</span>
+                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10 writing-content" id="card3">
+                            <h4><b>Watching Wise Leaders Deal With Complexity</b></h4> 
+                            <h6>By Prasad Kaipa and Navi Radjou</h6>
                         </div>
                     </div>      
                 </div> 
                 <!-- card 4 -->
-                 <div class="row" style="padding-bottom:4em;">
-                    <div class="col-lg-8 col-lg-offset-2" style="background-color:#f5f5f5;border:1px solid #D5D4D4;">
-                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10">
-                            <img src="/images/writingPage/PublishedArticles/ET.png" style="margin-left:-2.2em;width:100%;">
+                 <div class="row writing-card">
+                    <div class="col-lg-8 col-lg-offset-2 writing-cardRow">
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 writing-logos">
+                            <img src="/images/writingPage/PublishedArticles/ET.png">
                         </div>
-                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10" style="padding-top:1.4em;">
-                            <h4 style="line-height:1.2em;"><b>Ayurveda for Building Innovative Organizations</b></h4> 
-                            <h6 style="color:grey;">By Prasad Kaipa and Navi Radjou</h6>
-                            <span>Ayurveda philosophy can help corporations enhance productivity and creativity</span>
+                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10 writing-content" id="card4">
+                            <h4><b>Ayurveda for Building Innovative Organizations</b></h4> 
+                            <h6>By Prasad Kaipa and Navi Radjou</h6>
                         </div>
                     </div>      
                 </div> 
                 <!-- card 5 -->
-                <div class="row" style="padding-bottom:4em;">
-                    <div class="col-lg-8 col-lg-offset-2" style="background-color:#f5f5f5;border:1px solid #D5D4D4;">
-                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10">
-                            <img src="/images/writingPage/PublishedArticles/Fortune.png" style="margin-left:-2.2em;width:100%;">
+                <div class="row writing-card">
+                    <div class="col-lg-8 col-lg-offset-2 writing-cardRow">
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 writing-logos">
+                            <img src="/images/writingPage/PublishedArticles/Fortune.png">
                         </div>
-                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10" style="padding-top:1.4em;">
-                            <h4 style="line-height:1.2em;"><b>Apple and Tim Cook's next steps</b></h4> 
-                            <h6 style="color:grey;">By Prasad Kaipa and John Edson</h6>
-                            <span>Cheap, humbled, and now disruptive. This is the vector along which Apple is going to succeed.</span>
+                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10 writing-content" id="card5">
+                            <h4><b>Apple and Tim Cook's next steps</b></h4> 
+                            <h6>By Prasad Kaipa and John Edson</h6>
                         </div>
                     </div>      
                 </div>
                 <!-- card 6 --> 
-                <div class="row" style="padding-bottom:4em;">
-                    <div class="col-lg-8 col-lg-offset-2" style="background-color:#f5f5f5;border:1px solid #D5D4D4;">
-                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10">
-                            <img src="/images/writingPage/PublishedArticles/HBR.png" style="margin-left:-2.2em;width:100%;">
+                <div class="row writing-card">
+                    <div class="col-lg-8 col-lg-offset-2 writing-cardRow">
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 writing-logos">
+                            <img src="/images/writingPage/PublishedArticles/HBR.png">
                         </div>
-                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10" style="padding-top:1.4em;">
-                            <h4 style="line-height:1.2em;"><b>Recover Your Credibility</b></h4> 
-                            <h6 style="color:grey;">By Prasad Kaipa</h6>
-                            <span>The most effective way to start bridging the credibility gap is to be more aware of what you say.</span>
+                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10 writing-content" id="card6">
+                            <h4><b>Recover Your Credibility</b></h4> 
+                            <h6>By Prasad Kaipa</h6>
                         </div>
                     </div>      
                 </div>    
@@ -192,16 +187,5 @@
             </div>
         </div>            
     </div>
-</section>       -->
-
-<script type="text/javascript">
- function myOverFunction() {
-    document.getElementById("demo3").innerHTML = 'Make unpopular and counter-intuitive decisions.';
-}
-function outFunction(){
-
-}
-</script>
-
-
+</section> -->
 @stop

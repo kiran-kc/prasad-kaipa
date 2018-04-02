0f9fe9ca3a1ea0639e547dd425156c3aa5304651
Kiran Joshi <kiran@karmacircles.com>
2018-04-02 17:03:42 +0530

    Make writings blade responsive

--- resources/views/writings.blade.php
+++ resources/views/writings.blade.php
@@ -3,36 +3,46 @@
 <section class="writing-container">
   	<div class="container-fluid">
     	<div class="row writing-header">
-    		<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2 writing-box">
+           <div class="pseudo-navbar"></div> 
+    		<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-12 writing-box">
     			<header>
     				<h1 align="center">BOOKS</h1>
     				<hr align="center">
     			</header>
     			<div class="row writing-mdesign">
-		    		<div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 writing-book">
-		    			<img src="/images/writingPage/Books/Book1.png">
+		    		<div class="col-lg-4 col-md-5 col-sm-5 writing-book">
+		    			<img src="/images/writingPage/Books/Book1.png" align="center">
 		    		</div>
-		    		<div class="col-lg-7 col-lg-offset-1 col-md-8 col-md-offset-1 col-sm-6 col-sm-offset-1 col-xs-10 col-xs-offset-1 writing-content">
+                    <div class="col-xs-8 col-xs-offset-2 writing-bookMobile">
+                        <img src="/images/writingPage/Books/Book1.png" align="center">
+                    </div>
+		    		<div class="col-lg-8 col-md-7 col-sm-7 col-xs-12 writing-content">
 		    			<h2><b>You Can: Be Smarter and Wiser</b></h2> 
                         <h4><i>By Prasad Kaipa and Meera Shenoy</i></h4>
                         <h3>The real-life stories profiled in this book cover entrepreneurs ranging from some in wheel chairs to visually impaired persons who build their businesses with zest and energy. It looks at CEOs who hire youth with disability and make products for this market, because it makes business sense. And it celebrates those who have converted disability in their personal lives to an opportunity to transform other vulnerable lives.</h3> 
 		    		</div>
 		    	</div>
                 <div class="row writing-mdesign mdRow">
-                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 writing-book">
+                    <div class="col-lg-4 col-md-5 col-sm-5 writing-book">
                         <img src="/images/writingPage/Books/Book2.png">
                     </div>
-                    <div class="col-lg-7 col-lg-offset-1 col-md-8 col-md-offset-1 col-sm-6 col-sm-offset-1 col-xs-10 col-xs-offset-1 writing-content">
+                    <div class="col-xs-8 col-xs-offset-2 writing-bookMobile">
+                        <img src="/images/writingPage/Books/Book2.png">
+                    </div>
+                    <div class="col-lg-8 col-md-7 col-sm-7 col-xs-12 writing-content">
                         <h2><b>From Smart to Wise</b></h2> 
                         <h4><i>By Prasad Kaipa and Navi Radjou</i></h4>
                         <h3>From Smart to Wise offers a new approach that balances the two styles to achieve a form of wise leadership that is both functional and smart. Drawing on inspiring real-life stories of historical and contemporary wise leaders such as Bill Gates, Warren Buffet, and even Mahatma Gandhi, the authors identify six characteristics of wise leaders and offer a practical framework to help readers develop their own style of wise leadership.</h3> 
                     </div>
                 </div>
                 <div class="row writing-mdesign mdRow">
-                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 writing-book">
+                    <div class="col-lg-4 col-md-5 col-sm-5 writing-book">
                         <img src="/images/writingPage/Books/Book3.png">
                     </div>
-                    <div class="col-lg-7 col-lg-offset-1 col-md-8 col-md-offset-1 col-sm-6 col-sm-offset-1 col-xs-10 col-xs-offset-1 writing-content">
+                    <div class="col-xs-8 col-xs-offset-2 writing-bookMobile">
+                        <img src="/images/writingPage/Books/Book3.png">
+                    </div>
+                    <div class="col-lg-8 col-md-7 col-sm-7 col-xs-12 writing-content">
                         <h2><b>Discontinuous Learning: Igniting Genius Within by Aligning Self, Work, & Family (eBook)</b></h2> 
                         <h4><i>By Prasad Kaipa</i></h4>
                         <h3>In times of turbulence, a discontinuity, uncertainty and rapid change brings about a major shift in perspective. Discontinuous learning allows us to learn about ourselves, alters our mental models, and changes our relationship to the world.</h3> 
@@ -46,14 +56,14 @@
 <section class="writing-container">
     <div class="container-fluid">
          <div class="row writing-header">
-            <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2 writing-box">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-12 writing-box">
                 <header>
                     <h1 align="center">PUBLISHED ARTICLES</h1>
                     <hr align="center">
                 </header>
                 <!-- card 1 -->
                 <div class="row writing-card">
-                    <div class="col-lg-8 col-lg-offset-2 writing-cardRow" onmouseover="myOverFunction()"  onmouseout="outFunction()">
+                    <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 writing-cardRow" onmouseover="myOverFunction()"  onmouseout="outFunction()">
                         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 writing-logos">
                             <img src="/images/writingPage/PublishedArticles/CNBC.png">
                         </div>
@@ -65,11 +75,11 @@
                 </div> 
                 <!-- card 2 -->
                  <div class="row writing-card">
-                    <div class="col-lg-8 col-lg-offset-2 writing-cardRow">
-                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 writing-logos">
+                    <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 writing-cardRow">
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 writing-logos">
                             <img src="/images/writingPage/PublishedArticles/Forbes.png">
                         </div>
-                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10 writing-content" id="card2">
+                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 writing-content" id="card2">
                             <h4><b>How to Gain Role Clarity and Lead from any Position</b></h4> 
                             <h6>By Prasad Kaipa, Navi Radjou and Sarwath Khizrana</h6>
                         </div>
@@ -77,11 +87,11 @@
                 </div>
                 <!-- card 3  --> 
                  <div class="row writing-card">
-                    <div class="col-lg-8 col-lg-offset-2 writing-cardRow">
-                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 writing-logos">
+                    <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 writing-cardRow">
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 writing-logos">
                             <img src="/images/writingPage/PublishedArticles/HBR.png">
                         </div>
-                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10 writing-content" id="card3">
+                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 writing-content" id="card3">
                             <h4><b>Watching Wise Leaders Deal With Complexity</b></h4> 
                             <h6>By Prasad Kaipa and Navi Radjou</h6>
                         </div>
@@ -89,11 +99,11 @@
                 </div> 
                 <!-- card 4 -->
                  <div class="row writing-card">
-                    <div class="col-lg-8 col-lg-offset-2 writing-cardRow">
-                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 writing-logos">
+                    <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 writing-cardRow">
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 writing-logos">
                             <img src="/images/writingPage/PublishedArticles/ET.png">
                         </div>
-                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10 writing-content" id="card4">
+                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 writing-content" id="card4">
                             <h4><b>Ayurveda for Building Innovative Organizations</b></h4> 
                             <h6>By Prasad Kaipa and Navi Radjou</h6>
                         </div>
@@ -101,11 +111,11 @@
                 </div> 
                 <!-- card 5 -->
                 <div class="row writing-card">
-                    <div class="col-lg-8 col-lg-offset-2 writing-cardRow">
-                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 writing-logos">
+                    <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 writing-cardRow">
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 writing-logos">
                             <img src="/images/writingPage/PublishedArticles/Fortune.png">
                         </div>
-                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10 writing-content" id="card5">
+                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 writing-content" id="card5">
                             <h4><b>Apple and Tim Cook's next steps</b></h4> 
                             <h6>By Prasad Kaipa and John Edson</h6>
                         </div>
@@ -113,11 +123,11 @@
                 </div>
                 <!-- card 6 --> 
                 <div class="row writing-card">
-                    <div class="col-lg-8 col-lg-offset-2 writing-cardRow">
-                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 writing-logos">
+                    <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 writing-cardRow">
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 writing-logos">
                             <img src="/images/writingPage/PublishedArticles/HBR.png">
                         </div>
-                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10 writing-content" id="card6">
+                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 writing-content" id="card6">
                             <h4><b>Recover Your Credibility</b></h4> 
                             <h6>By Prasad Kaipa</h6>
                         </div>

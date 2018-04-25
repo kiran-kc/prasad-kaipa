7be69ce53e5b65dc34bb359fdda229a1ff478cf0
Kiran Joshi <kiran@karmacircles.com>
2018-04-25 18:03:47 +0530

    Add academia design with responsiveness

---
+++ resources/views/academia.blade.php
@@ -0,0 +1,66 @@
+@extends('common.master')
+@section('content')
+<section class="container-fluid" style="background-color:#EDEFF1;">
+	<div class="row academia-container pseudo-navbar">
+	<br><br><br><br>
+		<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-12 academia-gridlayout">
+			<div class="academia-cards leftcard">
+				<img src="/images/AcademiaLogos/isb.png">
+				<h4 class="imgMargin">Dr. Kaipa has offered executive education programs at Indian School of Business, Hyderabad since 2004.</h4>
+				<h4>He teaches Transformational Leadership: Tapping into Your Personal DNA. Through this program, Prasad shares his research into the nature of learning and thinking.</h4>
+				<h4>Find out more <a href="http://www.isb.edu/cee/TLDNA">here</a></h4>
+			</div>
+			<div class="academia-cards rightcard">
+				<img src="/images/AcademiaLogos/woxsen.png" class="small">
+				<h4 class="imgMargin">Dr. Kaipa is part of the Woxsen family in the capacity of Affiliate Faculty for Executive Education.</h4>
+				<h4>Woxsen's vision is to create the largest family of game changers worldwide that come together at any stage, be it the first day or several decades hence.</h4>
+				<h4>Find out more <a href="http://www.woxsen.edu.in/site/Faculty.asp?st=Prasad">here</a></h4>
+			</div>
+		</div>
+		<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-12 academia-gridlayout">
+			<div class="academia-cards leftcard">
+				<img src="/images/AcademiaLogos/soil.png">
+				<h4 class="imgMargin">Dr. Kaipa is on the Advisory Board of School of Inspired Leadership in Gurugram, India.</h4>
+				<h4>SOIL’s vision is to build Inspired Leaders with Character, Competence and Enthusiasm.</h4>
+				<p>Watch Prasad give a Leadership talk <a href="https://www.youtube.com/watch?v=guoE-YfS04E">here</a>.</p>
+				<h4>Find out more <a href="http://www.soilindia.net/about-soil/advisory-board">here</a></h4>
+			</div>
+			<div class="academia-cards rightcard">
+				<img src="/images/AcademiaLogos/saybrook.png" class="small">
+				<h4 class="imgMargin">Dr. Kaipa has been an integral part of the Leadership and Management Department at Saybrook University.</h4>
+				<h4>Saybrook students find a voice to articulate and confront the injustices that they witness and make positive change in the world.</h4>
+				<h4>Watch Prasad discuss his book with students in  <a href="https://www.youtube.com/watch?v=QFfu9c3ZjUw">this video.</a></h4>
+			</div>
+		</div>
+		<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-12 academia-gridlayout">
+			<h2 align="center">Business Schools Prasad has been associated with</h2>
+			<hr align="center">
+			<div class="academia-businesSchool">
+				<img src="/images/AcademiaLogos/insead.png" class="image">
+			</div>
+			<div class="academia-businesSchool">
+				<img src="/images/AcademiaLogos/iimb.png" class="image">
+			</div>
+			<div class="academia-businesSchool">
+				<img src="/images/AcademiaLogos/USC_transparent.png" class="usc"> 
+			</div>
+			<div class="academia-businesSchool mobileVisible">
+				<img src="/images/AcademiaLogos/tuck.png">
+			</div>
+			<div class="academia-businesSchool mobileVisible">
+				<img src="/images/AcademiaLogos/lbs.png" style="width:65%;">
+			</div>
+			<br><br>
+			<div align="center" class="webVisible">
+				<div class="academia-businesSchool">
+					<img src="/images/AcademiaLogos/tuck.png">
+				</div>
+				<div class="academia-businesSchool">
+					<img src="/images/AcademiaLogos/lbs.png" style="width:65%;">
+				</div>
+			</div>
+			<br><br><br><br>	 
+		</div>
+	</div>
+</section>
+@stop

2f7eb48aeef22755187cffc7d7b63278e4fd5da8
Kiran Joshi <kiran@karmacircles.com>
2018-05-03 16:53:33 +0530

    Fix speaking tab and remove code used for thumbnail

--- resources/views/speaking.blade.php
+++ resources/views/speaking.blade.php
@@ -4,9 +4,8 @@
 	<div class="container-fluid">
 		<div class="row videos-container pseudo-navbar">
 <!--================ video 1 Row 1 =============================-->
-			<div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 videos-gridLayout" style="margin-top:8em;">
-			<div class='youtube_codegena frame-type1'  style='width:100%;height:600px;' id='18O4DiTTxyo' data-params='?&theme=dark&autoplay=1&autohide=2' src='/images/speakings/Thumbnails/Masterclass.png'></div>
-			<script src='https://rawgit.com/shaneapen/Async-Youtube-Player/master/async_youtube_player.js'></script>
+			<div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-11 videos-gridLayout frame-top">
+				<iframe src="https://www.youtube.com/embed/18O4DiTTxyo" allowfullscreen class="frame-type1"></iframe>
  				<div class="videos-title">
  					<h3><strong>Masterclass on wise leadership in association with Univibe</strong></h3>
  					<h4>Dr. Kaipa talks about building wisdom in leadership. Access the full course 
@@ -15,21 +14,18 @@
  				</div>
  			</div>
 		</div>
+		<br>
 <!--================ video 2 and 3 Row 2 =============================-->		
 		<div class="row videos-container">
-			<div class="col-lg-5 col-lg-offset-1 col-md-5 col-md-offset-1 col-sm-5 col-sm-offset-1 col-xs-5 col-xs-offset-1 videos-gridLayout" align="left">
-				<!-- <div class='youtube_codegena' id='bnX4WaEwLC4' data-params='?&theme=dark&autoplay=1&autohide=2' src='/images/speakings/Thumbnails/AlanOlsenAmericanDreams.png' style='width:100%;height:390px;'></div> -->
-				<!-- <script src='https://rawgit.com/shaneapen/Async-Youtube-Player/master/async_youtube_player.js'></script> -->
-				<iframe src="https://www.youtube.com/embed/bnX4WaEwLC4" align="left" allowfullscreen class="frame-type2"></iframe>
+			<div class="col-lg-5 col-lg-offset-1 col-md-5 col-md-offset-1 col-sm-5 col-sm-offset-1 videos-gridLayout">
+				<iframe src="https://www.youtube.com/embed/bnX4WaEwLC4" allowfullscreen class="frame-type2"></iframe>
 				<div class="videos-title">
 		 			<h3><strong>Alan Olsen's American Dreams</strong></h3>
 		 			<h4>Dr. Kaipa explains that appreciative inquiry, honesty and authenticity trumps forced changes, every time.</h4>
 		 		</div>
 			</div>
-			<div class="col-lg-5 col-md-5 col-sm-5 col-xs-5 videos-gridLayout" align="right">
-				<div class='youtube_codegena' id='Bs-8dV5jPTE' data-params='?&theme=dark&autoplay=1&autohide=2' src='/images/speakings/Thumbnails/DrKaipaonGeneration.png' style='width:100%;height:413px;'></div>
-				<script src='https://rawgit.com/shaneapen/Async-Youtube-Player/master/async_youtube_player.js'></script>
- 				<!-- <iframe src="https://www.youtube.com/embed/Bs-8dV5jPTE" align="right" allowfullscreen class="frame-type2"></iframe>	 -->
+			<div class="col-lg-5 col-md-5 col-sm-5 videos-gridLayout" align="right">
+				<iframe src="https://www.youtube.com/embed/Bs-8dV5jPTE" align="right" allowfullscreen class="frame-type2"></iframe>	
 				<div class="videos-title" align="left">
 		 			<h3><strong>Dr. Kaipa on Generation Leader</strong></h3>
 		 			<h4>A candid chat about Leadership and its implications.</h4>
@@ -38,19 +34,15 @@
 		</div>
 <!--================ video 4 and 5 Row 3 =============================-->		
 		<div class="row videos-container">
-			<div class="col-lg-4 col-lg-offset-1 col-md-4 col-md-offset-1 col-sm-4 col-sm-offset-1 col-xs-4 col-xs-offset-1 videos-gridLayout" align="left">
-				<!-- <div class='youtube_codegena' id='rjsAHCpCoMU' data-params='?&theme=dark&autoplay=1&autohide=2' src='/images/speakings/Thumbnails/TalksatGoogle.png' style='width:100%;height:390px;'></div> -->
-				<!-- <script src='https://rawgit.com/shaneapen/Async-Youtube-Player/master/async_youtube_player.js'></script> -->
+			<div class="col-lg-4 col-lg-offset-1 col-md-4 col-md-offset-1 col-sm-4 col-sm-offset-1 videos-gridLayout" align="left">
 				<iframe src="https://www.youtube.com/embed/rjsAHCpCoMU" align="left" allowfullscreen class="frame-type3"></iframe>
 				<div class="videos-title">
 		 			<h3><strong>Talks at Google</strong></h3>
 		 			<h4>Dr. Kaipa's take on two extreme styles of Leadership.</h4>
 		 		</div>
 			</div>
-			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 videos-gridLayout" align="right">
-				<div class='youtube_codegena' id='o343JN-FHng' data-params='?&theme=dark&autoplay=1&autohide=2' src='/images/speakings/Thumbnails/TEDxBerkeley.png' style='width:100%;height:390px;'></div>
-				<script src='https://rawgit.com/shaneapen/Async-Youtube-Player/master/async_youtube_player.js'></script>
- 				<!-- <iframe src="https://www.youtube.com/embed/o343JN-FHng" align="right" allowfullscreen class="frame-type4"></iframe>	 -->
+			<div class="col-lg-6 col-md-6 col-sm-6 videos-gridLayout" align="right">
+ 				<iframe src="https://www.youtube.com/embed/o343JN-FHng" align="right" allowfullscreen class="frame-type4"></iframe>	
 				<div class="videos-title" align="left">
 		 			<h3><strong>TEDx Berkeley</strong></h3>
 		 			<h4>Keynote Speaker at TEDx Berkeley - Realizing your full potential.</h4>
@@ -59,10 +51,8 @@
 		</div>
 <!--================ video 6 Row 4 =============================-->		
 		<div class="row videos-container">
-			<div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 videos-gridLayout" align="left">
-				<div class='youtube_codegena' id='o343JN-FHng' data-params='?&theme=dark&autoplay=1&autohide=2' src='/images/speakings/Thumbnails/DrPrasadKaipaatTheHappiness.png' style='width:100%;height:600px;'></div>
-				<script src='https://rawgit.com/shaneapen/Async-Youtube-Player/master/async_youtube_player.js'></script>
-				<!-- <iframe src="https://www.youtube.com/embed/JL0QvrFsLho" align="center" allowfullscreen class="frame-type1"></iframe> -->
+			<div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 videos-gridLayout" align="left">
+				<iframe src="https://www.youtube.com/embed/JL0QvrFsLho" align="center" allowfullscreen class="frame-type1"></iframe>
 				<div class="videos-title">
 		 			<h3><strong>Dr Prasad Kaipa At The Happiness Hall Of Fame</strong></h3>
 		 			<h4>Finding passion, purpose and meaning can help us to be happy and supportive of each other. Dr. Kaipa gives an inspirational speech at The Happiness Hall of Fame.</h4>
@@ -71,17 +61,14 @@
 		</div>
 <!--================ video 7 and 8 Row 5 =============================-->			
 		<div class="row videos-container">
-			<div class="col-lg-6 col-lg-offset-1  col-md-6 col-md-offset-1 col-sm-6 col-sm-offset-1 col-xs-6 col-xs-offset-1  videos-gridLayout" align="left">
-				<!-- <div class='youtube_codegena' id='guoE-YfS04E' data-params='?&theme=dark&autoplay=1&autohide=2' src='/images/speakings/Thumbnails/Drprasadatschoolofinspired.png' style='width:100%;height:390px;'></div><script src='https://rawgit.com/shaneapen/Async-Youtube-Player/master/async_youtube_player.js'></script> -->
+			<div class="col-lg-6 col-lg-offset-1  col-md-6 col-md-offset-1 col-sm-6 col-sm-offset-1  videos-gridLayout" align="left">
 				<iframe src="https://www.youtube.com/embed/guoE-YfS04E" align="left" allowfullscreen class="frame-type4"></iframe>
 				<div class="videos-title">
 		 			<h3><strong>Dr Prasad Kaipa At School of Inspired Leadership</strong></h3>
 		 			<h4>Dr. Kaipa shares insights on Leadership, Innovation in modern times and his life's journey with students.</h4>
 		 		</div>
 			</div>
-			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 videos-gridLayout" align="right">
-				<!-- <div class='youtube_codegena' id='XhFTkPl7JCo' data-params='?&theme=dark&autoplay=1&autohide=2' src='/images/speakings/Thumbnails/Systemthinkinginaction.png' style='width:100%;height:390px;'></div> -->
-				<!-- <script src='https://rawgit.com/shaneapen/Async-Youtube-Player/master/async_youtube_player.js'></script> -->
+			<div class="col-lg-4 col-md-4 col-sm-4 videos-gridLayout" align="right">
  				<iframe src="https://www.youtube.com/embed/XhFTkPl7JCo" align="right" allowfullscreen class="frame-type3"></iframe>	
 				<div class="videos-title" align="left">
 		 			<h3><strong>System Thinking in Action Conference</strong></h3>
@@ -91,14 +78,14 @@
 		</div>	
 <!--================ video 9 and 10 Row 6 =============================-->		
 		<div class="row videos-container">
-			<div class="col-lg-5 col-lg-offset-1 col-md-5 col-md-offset-1 col-sm-5 col-sm-offset-1 col-xs-5 col-xs-offset-1 videos-gridLayout" align="left">
+			<div class="col-lg-5 col-lg-offset-1 col-md-5 col-md-offset-1 col-sm-5 col-sm-offset-1 videos-gridLayout" align="left">
 				<iframe src="https://www.youtube.com/embed/HD-yFYQ5UPM" align="left" allowfullscreen class="frame-type2"></iframe>
 				<div class="videos-title">
 		 			<h3><strong>Secrets of successful entrepreneurs</strong></h3>
 		 			<h4>Dr. Kaipa at TEDxBayArea on why some entrepreneurs fail and some succeed.</h4>
 		 		</div>
 			</div>
-			<div class="col-lg-5 col-md-5 col-sm-5 col-xs-5 videos-gridLayout" align="right">
+			<div class="col-lg-5 col-md-5 col-sm-5 videos-gridLayout" align="right">
  				<iframe src="https://www.youtube.com/embed/7dv19F4l4Gg" align="right" allowfullscreen class="frame-type2"></iframe>	
 				<div class="videos-title" align="left">
 		 			<h3><strong>Identify Your True North</strong></h3>
@@ -108,22 +95,6 @@
 		</div>
 	</div>		
 </section>
-
-<script type="text/javascript">
-
-$("document").ready(function(){
-	document.getElementsById('if-frame').style.display = "none";
-	document.getElementById("if-thumbnail").addEventListener("click", myFunction);
-
-
-	function myFunction() {
-    	document.getElementById("if-thumbnail").style.display="none";
-    	document.getElementById("if-frame").style.display="block";
-	}
-});
-
-
-</script>
 @stop
 
 
